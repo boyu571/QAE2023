a5a8e5af71419047dc9b932c475c209c92f06bcf
Ryeo Myung Kang <boyu571@gmail.com>
2023-06-03 15:02:01 +0900

    draft dashboard created

--- data_gathering.R
+++ data_gathering.R
@@ -6,24 +6,44 @@ library(readxl)
 library(binancer)
 
 
+today <- Sys.Date()
+yesterday <-today-1
+year <-format(Sys.Date(), "%Y")
+day <- format(Sys.Date(), "%d")
+month <- format(Sys.Date(), "%m")
+last_month <- as.numeric(month) -1
+if (as.numeric(last_month) <10 ) {
+  last_month <- str_c('0',last_month)
+} else {
+  last_month <-last_month
+}
+month_date
+as.numeric(day)
+if (as.numeric(day) >1 ) {
+    month_date <- format(Sys.Date(), "%m")
+} else {
+  month_date <- last_month
+}
+
 # 비트코인 시세(binance)
 ?binance_klines()
 # binance_klines('BTCUSDT', interval = '1d', start_time = '2017-01-01', end_time ='2017-12-31')
 
 stack <- NULL
-for (i in 2017:2023){
+for (i in 2017:year){
   annual <- binance_klines('BTCUSDT', interval = '1d', start_time = str_c(i,'-01-01'), end_time = str_c(i,'-12-31'))
   stack <- rbind(stack, annual)
 }
 View(stack)
 write_xlsx(stack, "binance.xlsx")
-
-library(ggplot2)
-ggplot(stack, aes(close_time, close)) + geom_line()
+# 
+# library(ggplot2)
+# ggplot(stack, aes(close_time, close)) + geom_line()
 
 
 # 달러지수
-URL <- "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=RTWEXBGS&scale=left&cosd=2006-01-01&coed=2023-04-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2023-05-10&revision_date=2023-05-10&nd=2006-01-01" 
+URL <- str_c("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=RTWEXBGS&scale=left&cosd=2006-01-01&coed=2023-",yesterday , "-02&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=",yesterday,"&revision_date=",yesterday,"&nd=2006-01-01")
+
 
 outfile <- "dollar_index.csv"
 download.file(URL, outfile)
@@ -44,7 +64,7 @@ write_xlsx(xauusd, "xauusd.xlsx")
 
 ### 나스닥 100
 
-URL <- "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=958&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=NASDAQ100&scale=left&cosd=1986-01-02&coed=2023-05-04&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily%2C%20Close&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2023-05-08&revision_date=2023-05-08&nd=1986-01-02"
+URL <- str_c("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=958&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=NASDAQ100&scale=left&cosd=2000-01-01&coed=", yesterday, "&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily%2C%20Close&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=", yesterday ,"&revision_date=", yesterday,"&nd=2000-01-01")
 
 outfile <- "nasdaq100.csv"
 download.file(URL, outfile)
@@ -52,13 +72,13 @@ download.file(URL, outfile)
 
 ### 연방 기금 금리
 
-URL <- "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=FEDFUNDS&scale=left&cosd=1954-07-01&coed=2023-04-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2023-05-10&revision_date=2023-05-10&nd=1954-07-01"
+URL <- str_c("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=FEDFUNDS&scale=left&cosd=2000-01-01&coed=", yesterday,"&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=", yesterday,"&revision_date=", yesterday,"&nd=1954-07-01")
 outfile <- "fedfunds.csv"
 download.file(URL, outfile)
 
 
 # 실업률
-URL <- "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=UNRATE&scale=left&cosd=1948-01-01&coed=2023-04-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2023-05-10&revision_date=2023-05-10&nd=1948-01-01"
+URL <- str_c("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=UNRATE&scale=left&cosd=2000-01-01&coed=", yesterday,"&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=", yesterday,"&revision_date=", yesterday,"&nd=1948-01-01")
 outfile <- "unem_rate.csv"
 download.file(URL, outfile)
 
@@ -70,17 +90,14 @@ download.file(URL, outfile)
 
 
 # CPI
-URL <- "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=CPIAUCSL&scale=left&cosd=1947-01-01&coed=2023-04-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2023-05-10&revision_date=2023-05-10&nd=1947-01-01"
+URL <- str_c("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=CPIAUCSL&scale=left&cosd=2000-01-01&coed=", yesterday,"&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=", yesterday,"&revision_date=", yesterday,"&nd=1947-01-01")
 outfile <- "CPI.csv"
 download.file(URL, outfile)
 
 
 # 국제 유가
-URL <- "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=DCOILWTICO&scale=left&cosd=1986-01-02&coed=2023-05-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2023-05-10&revision_date=2023-05-10&nd=1986-01-02"
+URL <- str_c("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1318&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=DCOILWTICO&scale=left&cosd=2000-01-02&coed=", yesterday,"&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=", yesterday,"&revision_date=", yesterday,"&nd=1986-01-02")
 outfile <- "oil.csv"
 download.file(URL, outfile)
-
-
-# 추후 Sys.Date()를 사용하여 자동화하고 전처리를 할 예정
 
 
